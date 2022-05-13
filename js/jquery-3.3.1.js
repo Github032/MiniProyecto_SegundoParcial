1b465fa9f9fa586403be9ef306abe60fd3323347
Arianna Ortega <43761216+Github032@users.noreply.github.com>
2022-05-13 00:30:06 -0500

    Add files via upload

---
+++ js/jquery-3.3.1.js
@@ -0,0 +1,8 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<!DOCTYPE plist PUBLIC "-//Apple//DTD PLIST 1.0//EN" "http://www.apple.com/DTDs/PropertyList-1.0.dtd">
+<plist version="1.0">
+<dict>
+	<key>URL</key>
+	<string>https://github.com/marioparra4/formularioAjax/blob/main/js/jquery-3.3.1.js</string>
+</dict>
+</plist>
